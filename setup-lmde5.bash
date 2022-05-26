3ad264504d12f8cef41358b28fd5b54468b36554
Jacob <89502494+jakespillstea@users.noreply.github.com>
2022-05-26 12:52:15 -0700

    Create setup-lmde5.bash

---
+++ setup-lmde5.bash
@@ -0,0 +1,6 @@
+# This is the bash script I will be using to configure LMDE5 at the office to be pure bliss and simplicity.
+sudo apt update
+sudo apt upgrade
+# reboot
+sudo apt install git htop vim chromium
+flatpak install 
