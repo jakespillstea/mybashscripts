76c64dcbae4dda33461cde29bdfa2a99602bed55
Jacob <89502494+jakespillstea@users.noreply.github.com>
2022-05-27 16:04:56 -0700

    Update setup-lmde5.bash

--- setup-lmde5.bash
+++ setup-lmde5.bash
@@ -1,6 +1,5 @@
 # This is the bash script I will be using to configure LMDE5 at the office to be pure bliss and simplicity.
-sudo apt update
-sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 # reboot
-sudo apt install git htop vim chromium
-flatpak install 
+sudo apt install git htop vim chromium cheese pdfsam
+flatpak install zoom pdfsam #onlyoffice
